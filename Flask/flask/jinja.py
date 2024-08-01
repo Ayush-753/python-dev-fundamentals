b1b871ccf3bcbf18e0d066d014580c3025590974
Ayush Kamath <ayushubkamath@gmail.com>
2024-08-01 23:07:42 +0530

    dynamic url

--- Flask/flask/jinja.py
+++ Flask/flask/jinja.py
@@ -10,7 +10,7 @@
 
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__)
 
@@ -26,13 +26,13 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route("/submit",methods=['GET','POST'])
-def suuuubmit():
+'''@app.route("/submit",methods=['GET','POST'])
+def submit():
     if request.method=='POST':
         name = request.form['name']
         return f'Hello {name}!'
     return render_template('form.html')
-
+'''
 ## Variable Rule
 @app.route("/success/<int:score>")
 def success(score):
@@ -57,5 +57,33 @@ def successres(score):
         
     return render_template('result1.html', results=exp)
 
+## if condition
+@app.route("/successif/<int:score>")
+def sucif(score):
+    
+    return render_template('result.html', results=score)
+ 
+ 
+@app.route("/fail/<int:score>")
+def fail(score):
+    
+    return render_template('result.html', results=res)
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresults.html')
+    return redirect(url_for('successres',score=total_score))
+        
+    
+  
 if __name__ == "___main__":
     app.run(debug=True)
